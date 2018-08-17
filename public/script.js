f4e24e9c066945074da134f163c51c276a8a13ac
Sarang Joshi <sarangj@msn.com>
2018-08-17 10:42:02 -0700

    Ready for demo

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
+const svgns = "http://www.w3.org/2000/svg";
+
 document.addEventListener('DOMContentLoaded', function() {
+    const HEIGHT = 400;
+    const WIDTH = 2.5 * HEIGHT;
+
+    document.getElementById('svg').setAttribute("height", HEIGHT);
+    document.getElementById('svg').setAttribute("width", WIDTH);
+    document.getElementById('map').setAttribute("height", HEIGHT);
+    document.getElementById('map').setAttribute("width", WIDTH);
+
     // Calculate the offset from the bottom-right corner
     const bl = {
         lat: 37.377021, long: -122.015133,
-    }
+    };
     const br = {
         lat: 37.376452, long: -122.015317,
     };
@@ -11,12 +21,9 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     const tr = {
         lat: 37.376394, long: -122.014993,
-    }
+    };
 
     try {
-        let app = firebase.app();
-        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-
         // Converts GPS location to point on map
         function convertLocationToPoint(location, fill) {
             const mapWidth = Math.sqrt(Math.pow(bl.long - br.long, 2) + Math.pow(bl.lat - br.lat, 2));
@@ -31,24 +38,43 @@ document.addEventListener('DOMContentLoaded', function() {
             const xPrime = z * Math.cos(alpha);
             const yPrime = z * Math.sin(alpha);
 
-            const right = xPrime * (400 / mapWidth);
-            const bottom = yPrime * (150 / mapHeight);
+            const right = xPrime * (WIDTH / mapWidth);
+            const bottom = yPrime * (HEIGHT / mapHeight);
 
-            const cx = 400 - right;
-            const cy = 150 - bottom;
+            const cx = WIDTH - right;
+            const cy = HEIGHT - bottom;
 
-            const svgns = "http://www.w3.org/2000/svg";
+            return {
+                cx, cy
+            };
+        }
+
+        function pointToCircle(p) {
             let circle = document.createElementNS(svgns, 'circle');
-            circle.setAttributeNS(null, 'cx', cx);
-            circle.setAttributeNS(null, 'cy', cy);
+            circle.setAttributeNS(null, 'cx', p.cx);
+            circle.setAttributeNS(null, 'cy', p.cy);
             circle.setAttributeNS(null, 'r', 10);
             if (fill) circle.setAttributeNS(null, 'fill', fill);
             document.getElementById('svg').appendChild(circle);
 
-            circles.push(circle);
+            elements.push(circle);
         }
 
-        let circles = [];
+        function pointToBen(p) {
+            const size = 100;
+
+            let ben = document.getElementById('ben');
+            ben.setAttributeNS(null, 'width', size);
+            ben.setAttributeNS(null, 'height', size);
+            ben.setAttributeNS(null, 'x', p.cx - size/2);
+            ben.setAttributeNS(null, 'y', p.cy - size/2);
+
+            let benCircle = document.getElementById('benCircle');
+            benCircle.setAttributeNS(null, 'cx', p.cx);
+            benCircle.setAttributeNS(null, 'cy', p.cy);
+        }
+
+        let elements = [];
 
         let db = firebase.database();
         let ref = db.ref('gpsLocation');
@@ -64,23 +90,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 long: -122.014895, lat: 37.376670,
             };
 
-            circles.forEach(c => {
+            elements.forEach(c => {
                 document.getElementById('svg').removeChild(c);
             });
-            circles = [];
+            elements = [];
 
-            convertLocationToPoint(frontDoor);
-            convertLocationToPoint(backDoor);
-            [bl, br, tl, tr].map(l => convertLocationToPoint(l));
+            // 6 points
+            // convertLocationToPoint(frontDoor);
+            // convertLocationToPoint(backDoor);
+            // [bl, br, tl, tr].map(l => convertLocationToPoint(l));
 
             // actual location
-            convertLocationToPoint(location, "blue");
+            pointToBen(convertLocationToPoint(location, "blue"));
 
             document.getElementById('gpsLocation').innerHTML = JSON.stringify(location);
         })
-        document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
     } catch (e) {
         console.error(e);
-        document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
     }
 });
